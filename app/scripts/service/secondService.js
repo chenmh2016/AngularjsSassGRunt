82d83de1a6ae11f9e8cee6511b1476dc94a69657
wuhuijie <whj768702@gmail.com>
2016-04-20 21:18:37 +0800

    漏合secondService.js文件。

---
+++ app/scripts/service/secondService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = angular.module('angularSassGruntApp');
+
+app.factory('service_F', function(){
+	var serviceInstance = function(value){
+		return 'factory service' + ' ' + value;
+	};	
+
+	return {
+		getNames:function(value){
+			return serviceInstance(value, 'getNames');
+		}
+	};
+});
