3adfa1ae70c832b47af6c7f647d31faa14407530
whj768702 <whj768702@gmail.com>
2016-04-21 08:03:47 +0800

    constant实现。

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,7 +8,7 @@
  * Controller of the angularSassGruntApp
  */
 angular.module('angularSassGruntApp')
-  .controller('AboutCtrl', ['$scope', 'service_S', 'service_F', function ($scope, service_S, service_F) {
+  .controller('AboutCtrl', ['$scope', 'service_S', 'service_F', 'serivce_C', function ($scope, service_S, service_F, service_C) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -20,4 +20,5 @@ angular.module('angularSassGruntApp')
     $scope.factoryService = service_F.getNames('hi');
     console.log(service_S.getName());
     console.log(service_F.getNames('World'));
+    $scope.constantConfig = service_C;
   }]);
--- app/scripts/service/constantService.js
+++ app/scripts/service/constantService.js
@@ -1,1 +1,7 @@
 'use strict';
+var app = angular.module('angularSassGruntApp');
+
+app.constant('serivce_C', {
+    config1: 'hello',
+    config2: 'i am from constant.'
+});
