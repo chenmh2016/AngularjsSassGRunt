b1b9a8c78b9fd306573543b1dd5d6a60cd5ea05a
whj768702 <whj768702@gmail.com>
2016-04-22 07:43:26 +0800

    添加constant说明。

--- app/scripts/service/constantService.js
+++ app/scripts/service/constantService.js
@@ -1,3 +1,8 @@
+/*
+ constant是个很有用的东东，我们经常会用于对directive之类的做配置信息。
+ 所以当你想创建一个directive，并且你希望能够做一些配置信息，同时给些默认的配置，constant是个不错的的选择。
+ constant可以译作常量，因为我们所设置的值value是不能被改变的。其可以接受基础类型和object对象
+*/
 'use strict';
 var app = angular.module('angularSassGruntApp');
 
