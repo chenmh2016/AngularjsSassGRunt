82580ec5681ddb0adc41c1cc5b70cea624657e96
c0505 <chenmh@dtdream.com>
2017-04-26 18:42:26 +0800

    将一些信息补充完全

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,16 +2,75 @@
 
 /**
  * @ngdoc function
- * @name angularSassGruntApp.controller:MainCtrl
+ * @name deployWebApp.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the angularSassGruntApp
+ * Controller of the deployWebApp
  */
 angular.module('deployWebApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    .controller('MainCtrl', controllerFn);
+    controllerFn.$inject = ['$scope', '$injector','$http'];
+    function controllerFn($scope, $injector,$http) {
+    $(function () {
+        $("[data-toggle='tooltip']").tooltip();
+    });
+    $scope.formData = {};
+    $scope.slaveNode = [{"ip": ''}];
+    $scope.vips = [{name: "vip_ageis"},
+        {name: "vip_cactus_batch"},
+        {name: "vip_cactus_keeper"},
+        {name: "vip_cas"},
+        {name: "vip_console"},
+        {name: "vip_defender"},
+        {name: "vip_diamond"},
+        {name: "vip_dts_all"},
+        {name: "vip_dts_console"},
+        {name: "vip_dubbo"},
+        {name: "vip_jmenv"},
+        {name: "vip_monitor"},
+        {name: "vip_mysql_yundun_1"},
+        {name: "vip_mysql_monitor"},
+        {name: "vip_redis"},
+        {name: "vip_sas"},
+        {name: "vip_secure"},
+        {name: "vip_uim"}
     ];
-  });
+    $scope.addInfomation = function () {
+        $scope.formData.ip_slave = $scope.slaveNode;
+        console.log(angular.toJson($scope.formData));
+    };
+    $scope.add = function () {
+        var arr = angular.copy($scope.slaveNode);
+        arr.push({"ip": ''});
+        $scope.slaveNode = arr;
+    };
+    $scope.delete=function(){
+        if(index==0){
+            delete $scope.ip[index];
+        }else {
+            $scope.ip.splice(index, 1);
+        }
+    };
+    $scope.run = function () {
+        $scope.formData.install_cloud_monitor_orNot = "no";
+        $scope.formData.isOS_aegisclient = "no";
+        $scope.formData.install_ntp_server_orNot = "yes";
+    };
+    $scope.run();
+
+    $scope.addInfomation=function(){
+        $http({
+            method: "GET",
+            url: "../../../../system/user/isLoginNameUsable.json?loginName=" + inputname
+        }).
+            success(function (data) {
+                if (data.success) {
+                    console.log("ok")
+                }
+                else {
+                    console.log("fail")
+                }
+            })
+    }
+
+}
--- app/scripts/filter/errorMessage.js
+++ app/scripts/filter/errorMessage.js
@@ -5,7 +5,8 @@ app.filter('error', function (){
         var message={
             required:'此项不能为空！',
             pattern:'请输入合法的Ip地址！',
-            same:'请保证输入的Ip地址与设定的网段相同！'
+            same:'请保证输入的Ip地址与设定的网段相同！',
+            email:'请输入正确的邮箱格式！'
         };
         return function(name){
             return message[name]||name;
